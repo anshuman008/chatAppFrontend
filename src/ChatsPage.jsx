a98430b6b1e838842b2c588eebdfd6bd13cf1938
Anshuman <107368860+anshuman008@users.noreply.github.com>
2024-03-08 19:55:58 +0530

    Add React app files and configuration

---
+++ src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={"f3e42857-8eb1-48e9-b744-9f6daee6a45d"}
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
